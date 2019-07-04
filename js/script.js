5e80e738eb6c86810eee8e4768fc0a3266064420
Piotr Fidler <fidler.piotr@gmail.com>
2019-07-04 09:33:43 +0200

    add Flickity plugin

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* Flickity plugin */
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
+  // options
+  hash: true,
+  contain: true,
+  wrapAround: true,
+  //autoPlay: 1500,
+  pageDots: false
+});
+
+document.querySelector(".restart-button").addEventListener("click", function() {
+  flkty.selectCell(".first");
+});
+
+var progressBar = document.querySelector(".progress-bar");
+flkty.on("scroll", function(progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + "%";
+});
